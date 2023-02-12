78f0bc356c1fbc756ae566295b2030fe3abafbfd
pankaj-kb <pankajb2601@gmail.com>
2023-02-12 23:31:36 +0530

    completed task 2

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ let newLogo = document.getElementsByClassName("logo")[0];
 console.log(newLogo)
 
 newLogo.src = "./assets/ineuron-logo.png";
+
+// Task 2 change price
+
+let pricePerMonth = document.querySelector(".app_price span");
+// console.log(pricePerMonth.innerText)
+
+pricePerMonth.innerText = "$10";
